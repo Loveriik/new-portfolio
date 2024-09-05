fe48b303c171a5bd8370340ceb48d26b4b438f3b
Aleksandr Lavrov <sancho2140@gmail.com>
2024-09-05 16:28:42 +0300

    t

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,10 @@ export default function Hero() {
         </h1>
         <div className="mt-8">
           <a
-            href={`${import.meta.env.BASE_URL}Aleksandr-Lavrov-CV.pdf`}
+            href={`${import.meta.env.BASE_URL}cv.pdf`}
             target="_blank"
             rel="noopener noreferrer"
-            download="Aleksandr-Lavrov-CV.pdf"
+            download="cv.pdf"
             className="flex items-center rounded-xl bg-lime-300 p-2 px-3 font-sans font-medium text-black hover:bg-lime-400"
           >
             <span>Resume.pdf</span>
